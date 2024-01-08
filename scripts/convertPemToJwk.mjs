ab09f83ea46fe0ea7882fc62dcc6f963e825944f
Sameer kumar <2006Sameer2009@gmail.com>
2024-01-08 13:19:49 +0530

    host public key

---
+++ scripts/convertPemToJwk.mjs
@@ -0,0 +1,10 @@
+import fs from "fs";
+import rsaPemToJwk from "rsa-pem-to-jwk";
+
+
+const privateKey = fs.readFileSync("./certs/privateKey.pem",);
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+const Jwk = rsaPemToJwk(privateKey,{use:"sig"}, "public")
+
+console.log(JSON.stringify(Jwk))
--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import logger from "./config/logger";
 import authRouter from "./routes/Auth.Routes";
 
 const app = express();
-
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -16,7 +16,7 @@ app.use("/auth", authRouter);
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
     logger.error(err.message);
-    const statusCode = err.statusCode || 500;
+    const statusCode = err.statusCode || err.status || 500;
     res.status(statusCode).json({
         errors: [
             {
