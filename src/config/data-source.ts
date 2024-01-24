1a9a3bdeee4a82a2b850b2b290191cb5c6093e5a
Sameer kumar <2006Sameer2009@gmail.com>
2024-01-24 08:28:01 +0530

    small change for typescript build

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -2,15 +2,6 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Config } from "./config";
 
-import { User } from "../entity/User.entity";
-import { UserRefreshToken } from "../entity/User.RefreshToken.entity";
-
-import { Seller } from "../entity/Seller.entity";
-import { SellerRefreshToken } from "../entity/Seller.RefreshToken.entity";
-
-import { Admin } from "../entity/Admin.entity";
-import { AdminRefreshToken } from "../entity/Admin.RefreshToken.entity";
-
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: Config.DB_HOST,
@@ -21,14 +12,7 @@ export const AppDataSource = new DataSource({
     // Dont't use this in production. Always keep false
     synchronize: false,
     logging: false,
-    entities: [
-        User,
-        UserRefreshToken,
-        Seller,
-        SellerRefreshToken,
-        Admin,
-        AdminRefreshToken,
-    ],
+    entities: ["src/entity/*{ts,js}"],
     migrations: ["src/migration/*.ts"],
     subscribers: [],
 });
