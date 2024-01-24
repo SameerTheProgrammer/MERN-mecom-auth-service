b3e9c2715368c9911b10ee0fd7bb2e3a6e304ea2
Sameer kumar <2006Sameer2009@gmail.com>
2024-01-24 08:40:27 +0530

    setup test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,12 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     verbose: true,
+    collectCoverage:true,
+    coverageProvider:"v8",
+    collectCoverageFrom:[
+        "src/**/*.ts",
+        "!tests/**",
+        "!**/node_modules/**"
+    ]
     // modulePathIgnorePatterns: ["/tests/sellers", "/tests/users"],
 };
